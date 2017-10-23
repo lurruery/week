762dfe3f5e6bd0a3d65f1020097265625e4d2127
ZhangMei [张梅] <zhangmeife@xiaomi.com>
2017-10-23 10:53:13 +0800

    update

--- bigData/num.js
+++ bigData/num.js
@@ -2,22 +2,3 @@
  * @param  string   line 单行测试数据
  * @return string        返回处理结果
  */
-
-/* 方法一 */
-function solution(line) {
-  var lineArray = line.split(' ');
-  var sum = lineArray[0];
-  var i = 0;
-  var flagArray = [];
-  for(var j = 1; j < line.length; j++) {
-    sum = lineArray[i];
-    if(sum === lineArray[j] || flagArray.indexOf(sum) !== -1) {
-      i ++;
-      j = i;
-      if(flagArray.indexOf(sum) === -1) {
-        flagArray.push(sum);
-      }
-    }
-  }
-  return Number(sum);
-}
